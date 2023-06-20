d37398cde167b408ad89f04946564ce2f0386308
halimabashir <93261455+halimabashir@users.noreply.github.com>
2023-06-20 12:47:19 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, jsonify
+import nltk
+#nltk.download('stopwords')
+import pickle
+from nltk.corpus import stopwords
+import re
+from nltk.stem.porter import PorterStemmer
+
+app = Flask(__name__)
+ps = PorterStemmer()
+
+with open ('pickle_model.pkl', 'rb') as file:
+    pickle_model = pickle.load(file)
+tfidfvect = pickle.load(open('tfidfvect.pkl', 'rb'))
+
+
+def predict(text):
+    review = re.sub('[^a-zA-Z]', ' ', str(text))
+    review = review.lower()
+    review = review.split()
+    review = [ps.stem(word) for word in review if not word in stopwords.words('english')]
+    review = ' '.join(review)
+    review_vect = tfidfvect.transform([review]).toarray()
+    prediction = 'FAKE' if pickle_model.predict(review_vect) == 0 else 'REAL'
+    return prediction
+
+
+@app.route('/predict', methods=['POST'])
+def api():
+
+    if request.method == 'POST':
+        text = request.form.get("text")
+        prediction = predict(text)
+        return jsonify(prediction=prediction)
+
+if __name__ == "__main__":
+    app.run(debug=True)
